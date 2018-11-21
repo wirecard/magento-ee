18d14c876c5bc9fd22d0aef9da6a8f026fb7c8c0
nehalist <github@nehalist.io>
2018-11-21 16:30:17 +0100

    Hide payment logos on checkout

--- app/code/community/WirecardEE/PaymentGateway/Block/Form.php
+++ app/code/community/WirecardEE/PaymentGateway/Block/Form.php
@@ -16,25 +16,25 @@ class WirecardEE_PaymentGateway_Block_Form extends Mage_Payment_Block_Form
     }
 
     /**
-     * Add payment logo to checkout.
+     * Add payment logo to checkout. Keep in mind that logos are not not shown by default; to show them uncomment the
+     * body of this method.
      *
      * @return string
      */
     public function getMethodLabelAfterHtml()
     {
-        $imagePath = 'images/WirecardEE/PaymentGateway/';
-        $imageName = \Mage::helper('catalog')
-                          ->__(str_replace('wirecardee_paymentgateway_', '', $this->getMethodCode()) . '.png');
-        $filePath  = sprintf(
-            '%s/frontend/base/default/%s', Mage::getBaseDir('skin'),
-            $imagePath . $imageName
-        );
-
-        if (file_exists($filePath)) {
-            $image = $this->getSkinUrl($imagePath . $imageName);
-            return "<img src='$image' style='float: initial !important;'>";
-        }
-
+        // $imagePath = 'images/WirecardEE/PaymentGateway/';
+        // $imageName = \Mage::helper('catalog')
+        //                      ->__(str_replace('wirecardee_paymentgateway_', '', $this->getMethodCode()) . '.png');
+        // $filePath  = sprintf(
+        //      '%s/frontend/base/default/%s', Mage::getBaseDir('skin'),
+        //      $imagePath . $imageName
+        // );
+        //
+        // if (file_exists($filePath)) {
+        //      $image = $this->getSkinUrl($imagePath . $imageName);
+        //      return "<img src='$image' style='float: initial !important;'>";
+        // }
         return '';
     }
 }
