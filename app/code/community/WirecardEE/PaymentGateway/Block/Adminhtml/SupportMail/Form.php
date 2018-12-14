dd516f33d1c2f3d8d6f33374afcb69f4422f95ca
Georg <gp@loremipsum.at>
2018-12-14 14:46:15 +0100

    coding standard

--- app/code/community/WirecardEE/PaymentGateway/Block/Adminhtml/SupportMail/Form.php
+++ app/code/community/WirecardEE/PaymentGateway/Block/Adminhtml/SupportMail/Form.php
@@ -1,20 +1,36 @@
 <?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/LICENSE
+ */
 
+/**
+ * @since 1.0.0
+ * @codingStandardsIgnoreStart
+ */
 class WirecardEE_PaymentGateway_Block_Adminhtml_SupportMail_Form extends Mage_Adminhtml_Block_Widget_Form
 {
-	protected function _prepareForm()
-	{
-        $form = new Varien_Data_Form(array(
-            'id' => 'edit_form',
-            'action' => $this->getUrl('*/*/send'),
-            'method' => 'post',
-            'enctype' => 'multipart/form-data'
-        )
+    protected function _prepareForm()
+    {
+        // @codingStandardsIgnoreEnd
+        $form = new Varien_Data_Form(
+            array(
+                'id' => 'edit_form',
+                'action' => $this->getUrl('*/*/send'),
+                'method' => 'post',
+                'enctype' => 'multipart/form-data',
+            )
         );
         $form->setUseContainer(true);
         $this->setForm($form);
 
-        $fieldset = $form->addFieldset('supportmail_form',array('legend'=>Mage::helper('paymentgateway')->__('Item information')));
+        $fieldset = $form->addFieldset(
+            'supportmail_form',
+            array('legend'=>Mage::helper('paymentgateway')->__('Item information'))
+        );
         $fieldset->addField('sender_address', 'text', array(
             'label' => Mage::helper('paymentgateway')->__('Sender Address'),
             'class' => 'required-entry',
@@ -37,5 +53,5 @@ class WirecardEE_PaymentGateway_Block_Adminhtml_SupportMail_Form extends Mage_Ad
         ));
 
         return parent::_prepareForm();
-	}
+    }
 }
