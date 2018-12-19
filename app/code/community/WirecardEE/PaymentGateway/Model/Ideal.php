ff7d0d5af17f95af0608b3ba093a8374ee917e99
Georg <gp@loremipsum.at>
2018-12-19 13:08:48 +0100

    ideal config

---
+++ app/code/community/WirecardEE/PaymentGateway/Model/Ideal.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/LICENSE
+ */
+
+use Wirecard\PaymentSdk\Transaction\Operation;
+use Wirecard\PaymentSdk\Transaction\IdealTransaction;
+
+/**
+ * @since 1.0.0
+ */
+class WirecardEE_PaymentGateway_Model_Ideal extends WirecardEE_PaymentGateway_Model_Payment
+{
+    protected $_code = 'wirecardee_paymentgateway_ideal';
+    protected $_paymentMethod = IdealTransaction::NAME;
+
+    /**
+     * Return available transaction types for this payment.
+     *
+     * @return array
+     *
+     * @since 1.0.0
+     */
+    // public function toOptionArray()
+    // {
+    //     return [
+    //         [
+    //             'value' => Operation::RESERVE,
+    //             'label' => Mage::helper('catalog')->__('Authorization'),
+    //         ],
+    //         [
+    //             'value' => Operation::PAY,
+    //             'label' => Mage::helper('catalog')->__('Purchase'),
+    //         ],
+    //     ];
+    // }
+}
