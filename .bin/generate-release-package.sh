91467a47ddfd74456ef843f80c11e9c7c277edba
Richard Blechinger <me@blechi.at>
2019-06-13 10:48:41 +0200

    Set up release package generation

--- .bin/generate-release-package.sh
+++ .bin/generate-release-package.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# This script will generate release package and keep composer.json if 'test' parameter is passed
 
 TARGET_DIRECTORY="."
 
 composer install --no-dev --prefer-dist
-if [[ $1 == 'test' ]]; then
-    zip -r magento-wirecard-ee.zip ${TARGET_DIRECTORY} composer.json -x "*tests*" -x "*Test*" -x "*codeception*"
-else
-    zip -r magento-wirecard-ee.zip ${TARGET_DIRECTORY} -x "*tests*" -x "*Test*" -x "*codeception*"
-fi
-
+zip -r magento-wirecard-ee.zip ${TARGET_DIRECTORY} composer.json \
+    -x "tests*" \
+    -x "phpcs.xml*" \
+    -x "phpunit.xml*" \
+    -x "README.md*" \
+    -x "SHOPVERSIONS*" \
+    -x ".bin*" \
+    -x ".gitignore*" \
+    -x ".editorconfig*" \
+    -x ".travis.yml"
