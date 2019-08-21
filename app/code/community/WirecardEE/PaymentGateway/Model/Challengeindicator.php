93831e044d5b021e98a2e0b14f544595567eec0f
Michael Bretterklieber <github@bretterklieber.com>
2019-08-21 10:50:07 +0200

    new config option challenge_indicator

---
+++ app/code/community/WirecardEE/PaymentGateway/Model/Challengeindicator.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/LICENSE
+ */
+
+use Wirecard\PaymentSdk\Constant\ChallengeInd;
+
+/**
+ * @since 1.2.4
+ */
+class WirecardEE_PaymentGateway_Model_Challengeindicator
+{
+    /**
+     * Return available challenge indicators
+     *
+     * @return array
+     *
+     * @since 1.2.4
+     */
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => ChallengeInd::NO_PREFERENCE,
+                'label' => Mage::helper('catalog')->__('config_challenge_indicator_no_preference'),
+            ],
+            [
+                'value' => ChallengeInd::NO_CHALLENGE,
+                'label' => Mage::helper('catalog')->__('config_challenge_indicator_no_challenge'),
+            ],
+            [
+                'value' => ChallengeInd::CHALLENGE_THREED,
+                'label' => Mage::helper('catalog')->__('config_challenge_indicator_threed'),
+            ],
+        ];
+    }
+
+}
