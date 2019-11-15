97e219a1af5f822a204f15f70f05aeec0227f032
serucee <sebastian.russmann@wirecard.com>
2019-11-15 10:46:41 +0100

    Add Przelewy24

---
+++ app/code/community/WirecardEE/PaymentGateway/Model/Ptwentyfour.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/magento-ee/blob/master/LICENSE
+ */
+
+use Wirecard\PaymentSdk\Transaction\PtwentyfourTransaction;
+
+/**
+ * @since 2.0.0
+ */
+class WirecardEE_PaymentGateway_Model_Ptwentyfour extends WirecardEE_PaymentGateway_Model_Payment
+{
+    protected $_code = 'wirecardee_paymentgateway_ptwentyfour';
+    protected $_paymentMethod = PtwentyfourTransaction::NAME;
+}
