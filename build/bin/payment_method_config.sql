ea2c3d9cf0bef1b78539e6b5c2903d96d009dee2
serucee <sebastian.russmann@wirecard.com>
2019-11-19 12:59:07 +0100

    Fix p24 config

--- build/bin/payment_method_config.sql
+++ build/bin/payment_method_config.sql
@@ -175,5 +175,6 @@ INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('defa
 INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('default',0,'payment/wirecardee_paymentgateway_ptwentyfour/api_password','3!3013=D3fD8X7');
 INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('default',0,'payment/wirecardee_paymentgateway_ptwentyfour/api_maid','86451785-3ed0-4aa1-99b2-cc32cf54ce9a');
 INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('default',0,'payment/wirecardee_paymentgateway_ptwentyfour/api_secret','fdd54ea1-cef1-449a-945c-55abc631cfdc');
+INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('default',0,'payment/wirecardee_paymentgateway_ptwentyfour/fraud_prevention','1');
 INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('default',0,'payment/wirecardee_paymentgateway_ptwentyfour/order_identification','1');
 INSERT INTO `core_config_data` (`scope`,`scope_id`,`path`,`value`) VALUES ('default',0,'payment/wirecardee_paymentgateway_ptwentyfour/sort_order','30');
